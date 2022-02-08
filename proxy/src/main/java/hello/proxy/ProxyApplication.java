43b2b6d9d1baf7aa0d6147d2d3c4c36bb1f9f999
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2022-02-08 14:48:58 +0900

    [2022.02.08] @Aspect 예제

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -10,6 +10,7 @@ import hello.proxy.config.v3_proxyfactory.ProxyFaceotyConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFaceotyConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -28,7 +29,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ProxyFaceotyConfigV1.class)
 //@Import(ProxyFaceotyConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
