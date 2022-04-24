f35da36e93c244cd818f2f04b07fee9839f92f60
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2022-04-24 21:05:08 +0900

    item02 예제

---
+++ effective-java-example/src/main/java/com/example/effectivejavaexample/item02/NutritionFacts.java
@@ -0,0 +1,88 @@
+package com.example.effectivejavaexample.item02;
+
+public class NutritionFacts {
+
+  private final int servingSize; // 필수
+  private final int servings; // 필수
+  private final int calories;
+  private final int fat;
+  private final int sodium;
+  private final int carbohydrate;
+
+  public NutritionFacts(int servingSize, int servings) {
+    this(servingSize, servings, 0);
+  }
+
+  public NutritionFacts(int servingSize, int servings, int calories) {
+    this(servingSize, servings, calories, 0);
+  }
+
+  public NutritionFacts(int servingSize, int servings, int calories, int fat) {
+    this(servingSize, servings, calories, fat, 0);
+  }
+
+  public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium) {
+    this(servingSize, servings, calories, fat, sodium, 0);
+  }
+
+  public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium,
+      int carbohydrate) {
+    this.servingSize = servingSize;
+    this.servings = servings;
+    this.calories = calories;
+    this.fat = fat;
+    this.sodium = sodium;
+    this.carbohydrate = carbohydrate;
+  }
+
+  public static class Builder {
+
+    // 필수 매개변수
+    private final int servingSize;
+    private final int servings;
+
+    // 선택 매개변수 - 기본값으로 초기화한다.
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public Builder(int servingSize, int servings) {
+      this.servingSize = servingSize;
+      this.servings = servings;
+    }
+
+    public Builder calories(int val) {
+      calories = val;
+      return this;
+    }
+
+    public Builder fat(int val) {
+      fat = val;
+      return this;
+    }
+
+    public Builder sodium(int val) {
+      sodium = val;
+      return this;
+    }
+
+    public Builder carbohydrate(int val) {
+      carbohydrate = val;
+      return this;
+    }
+
+    public NutritionFacts build() {
+      return new NutritionFacts(this);
+    }
+  }
+
+  private NutritionFacts(Builder builder) {
+    this.servingSize = builder.servingSize;
+    this.servings = builder.servings;
+    this.calories = builder.calories;
+    this.fat = builder.fat;
+    this.sodium = builder.sodium;
+    this.carbohydrate = builder.carbohydrate;
+  }
+}
