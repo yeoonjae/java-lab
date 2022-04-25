82d6b74f8d85714feb5b451f12771620a5bde0dd
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2022-04-25 21:47:24 +0900

    JPA 실습 프로젝트 생성

---
+++ spring-jpa/src/test/java/com/study/springjpabasic/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springjpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
